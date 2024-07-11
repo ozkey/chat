81b35753cf9dd319b02bf4c7bc6e1645d01cb3f6
oscar aguilera <ozkey@ozkey.com>
2024-07-11 20:32:21 +1000

    init

--- docs/index.js
+++ docs/index.js
@@ -6,12 +6,15 @@ function mail(){
     var inputElement = document.getElementById('milli');
     var inputValue = inputElement.value;
 
+    // clear input value
+    inputElement.value = '';
 
     if (isNaN(inputValue)) {
         inputValue = 30000
     }else{
         inputValue = Number(inputValue);
     }
+
     if (inputValue< 10 ) inputValue = 10000
     console.log(inputValue)
     setTimeout( play,inputValue)
