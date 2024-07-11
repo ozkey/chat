c2b78872b6727a18f4c060b84a21c6da4ea276f9
oscar aguilera <ozkey@ozkey.com>
2024-07-11 20:10:03 +1000

    init

---
+++ docs/index.js
@@ -0,0 +1,18 @@
+function play() {
+    var audio = new Audio('mail.mp3');
+    audio.play();
+}
+function mail(){
+    var inputElement = document.getElementById('milli');
+    var inputValue = inputElement.value;
+
+
+    if (isNaN(inputValue)) {
+        inputValue = 30000
+    }else{
+        inputValue = Number(inputValue);
+    }
+    if (inputValue< 10 ) inputValue = 10000
+    console.log(inputValue)
+    setTimeout( play,inputValue)
+}
